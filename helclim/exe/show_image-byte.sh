dfd22331c216156628e073563f028dad898fea8c
lee <lee@isi-solutions.org>
2012-08-07 12:17:20 +0200

    Changed .gitignore and removed several binaries that would have slowed cloning, etc.

--- helclim/exe/show_image-byte.sh
+++
@@ -1,4 +0,0 @@
-
-# Cloud Index und Grundalbedo Fulldisk vis008
-rawtopgm -bpp 1 -maxval 255 -headerskip 256 5000 5000 $1 > tmp.pgm
-xv tmp.pgm
--- helclim/exe/show_image-mfgN.sh
+++
@@ -1,4 +0,0 @@
-
-# Cloud Index und Grundalbedo Fulldisk vis008
-rawtopgm -bpp 2 -maxval 1024 -headerskip 256 5000 5000 $1 > tmp.pgm
-xv tmp.pgm
