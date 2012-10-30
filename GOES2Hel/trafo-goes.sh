46ce86b9e0fd244bcfa3958f09d5387bb1bb413f
lee <lee@isi-solutions.org>
2012-10-30 08:51:42 +0100

    Comment regridding control script
    This moves many descriptions from the readme file into the control
    script, making it easier to adapt. Also, the paths are adapted to my
    computer.
    
    In the long term, this should be done differently (i.e. the possibility
    of name parsing, etc.), but for now let's just get it to work :)

--- GOES2Hel/trafo-goes.sh
+++ GOES2Hel/trafo-goes.sh
@@ -1,18 +1,34 @@
+# Set month and year
 moyear=062003
-for day in 152 153 154 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 179 180 181 
-do 
-infile="goes09.2003."$day".012514.BAND_01.nc"
-outcdl="goes09.2003."$day".0125.cdl"
-out="goes09.2003."$day".0125.out"
-echo "\"/cmsaf/cmsaf-rad2/data/GOESraw/gms-062003_low_res/$infile\"" > input-cdl.asc
-echo "\"/cmsaf/cmsaf-rad2/data/GOESraw/gms-062003_low_res/info.log\"" >> input-cdl.asc
-echo "5208 2706" >> input-cdl.asc
-echo "1" >> input-cdl.asc
-echo "26 255 2003" >> input-cdl.asc
-echo "-1000" >> input-cdl.asc
-echo "\"/cmsaf/cmsaf-rad2/data/GOESraw/regnc/$outcdl\"" >>  input-cdl.asc
-echo  "\"/cmsaf/cmsaf-rad2/data/GOESraw/regnc/$out\"" >>  input-cdl.asc
-echo "95 180 -62 62  0.04  0.04" >>  input-cdl.asc
-echo "160 0.0" >> input-cdl.asc
-GOESnc2Hel.exe
+# Set days to be processed
+for day in 152 #153 154 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 179 180 181 
+do
+	# Set hours
+	for hour in 00 #01 02 03 04 05 06 07 08 09 11 12 13 14 15 16 17 18 19 20 21 22 23
+	do
+		# Set input paths
+		infile="goes09.2003."$day"."$hour"2514.BAND_01.nc"
+		outcdl="goes09.2003."$day"."$hour"25.cdl"
+		out="goes09.2003."$day"."$hour"25.out"
+		echo "\"/home/lee/Downloads/goes/raw/$infile\"" > input-cdl.asc
+		echo "\"/home/lee/Downloads/goes/raw/info.log\"" >> input-cdl.asc
+		# Set xdim and ydim (can be retrieved with ncdump -h)
+		echo "20812 10819" >> input-cdl.asc
+		# Set number of regions, do not change
+		echo "1" >> input-cdl.asc
+		# Scan time, highest possible value (dependent on bit length) and year of scan
+		echo "26 65535 2003" >> input-cdl.asc
+		# No data value, do not change
+		echo "-1000" >> input-cdl.asc
+		# Set output paths
+		echo "\"/home/lee/Downloads/goes/raw_processed/$outcdl\"" >>  input-cdl.asc
+		echo  "\"/home/lee/Downloads/goes/raw_processed/$out\"" >>  input-cdl.asc
+		# Set dimensions: xmin, xmax, ymin, ymax, and resolution in lat/long
+		# Note: The program can't deal with values going over the 180° border
+		# For <180° use GMS coverage, >-180° uses GOES-W coverage
+		echo "95 180 -62 62  0.01  0.01" >>  input-cdl.asc
+		# These values are only defined for testing
+		echo "160 0.0" >> input-cdl.asc
+		GOESnc2Hel.exe
+	done
 done
